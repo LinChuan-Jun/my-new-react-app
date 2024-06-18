2aca156cfb63dfffd3cacce2c9e537aa86308073
Lin, Chuan-Jun <144259037+LinChuan-Jun@users.noreply.github.com>
2024-06-19 03:53:07 +0800

    0

--- src/components/calculator/MyCalculator.js
+++ src/components/calculator/MyCalculator.js
@@ -1,13 +1,20 @@
+// 從 React 套件中引入 React 和 useState 函數
 import React, { useState } from "react";
+// 引入自定義的 MyDisplay 元件
 import MyDisplay from "./MyDisplay";
+// 引入自定義的 MyButton 元件
 import MyButton from "./MyButton";
+// 引入外部的 CSS 文件
 import "./MyCalculator.css";
 
+// 定義一個名為 MyCalculator 的函數元件
 function MyCalculator() {
+  // 使用 useState 函數來創建一個名為 result 的狀態變量，初始值為 "0"
   const [result, setResult] = useState("0");
-  // handleClick是MyButton的事件處理常式
+
+  // 定義一個名為 handleClick 的函數，用於處理按鈕的點擊事件
   const handleClick = (value) => {
-    // alert(value);
+    // 根據點擊的按鈕值進行不同的處理
     switch (value) {
       case "0":
       case "1":
@@ -24,6 +31,8 @@ function MyCalculator() {
       case "-":
       case "*":
       case "/":
+        // 如果 result 的值為 "0"，則將其設置為按鈕的值
+        // 否則，將按鈕的值追加到 result 之後
         if (result === "0") {
           setResult(value);
         } else {
@@ -31,27 +40,36 @@ function MyCalculator() {
         }
         break;
       case "=":
+        // 使用 eval 函數計算 result 的值，並將結果設置為新的 result
         setResult(eval(result).toString());
         break;
       case "c":
+        // 清空 result 的值
         setResult("");
         break;
       case "+/-":
+        // 將 result 的值轉換為數字，取反後再設置為新的 result
         setResult(parseInt(result, 10) * -1);
         break;
       case "%":
+        // 將 result 的值轉換為數字，除以 100 後再設置為新的 result
         setResult(parseInt(result, 10) / 100);
         break;
       default:
         break;
     }
   };
+
+  // 返回 JSX，這部分代碼將會渲染到網頁上
   return (
     <div className="calculator">
-      <MyDisplay result = {result} />
+      {/* 渲染 MyDisplay 元件，並將 result 狀態變量的值傳遞給它 */}
+      <MyDisplay result={result} />
+      {/* 渲染 MyButton 元件，並將 handleClick 函數作為 buttonClicked 屬性傳遞給它 */}
       <MyButton buttonClicked={handleClick} />
     </div>
   );
 }
 
+// 將 MyCalculator 函數元件作為預設導出
 export default MyCalculator;
--- src/components/tic-tac-toe/Board.js
+++ src/components/tic-tac-toe/Board.js
@@ -1,21 +1,31 @@
+// 從 React 套件中引入 React 函數
 import React from "react";
+// 引入自定義的 Square 元件
 import Square from "./Square";
+// 引入外部的 CSS 文件
 import "./tictactoe.css";
 
+// 定義一個名為 Board 的函數元件，接收 xIsNext、squares 和 onPlay 作為屬性
 function Board({ xIsNext, squares, onPlay }) {
+  // 定義一個名為 handleClick 的函數，用於處理每個方格的點擊事件
   const handleClick = (i) => {
+    // 如果已經有贏家或者該方格已經被點擊過，則直接返回
     if (calculateWinner(squares) || squares[i]) {
       return;
     }
+    // 創建下一步的 squares 狀態
     const nextSquares = squares.slice();
+    // 根據 xIsNext 的值設置下一步的方格為 "X" 或 "O"
     if (xIsNext) {
       nextSquares[i] = "X";
     } else {
       nextSquares[i] = "O";
     }
+    // 調用 onPlay 函數並傳遞 nextSquares
     onPlay(nextSquares);
   };
 
+  // 定義一個名為 calculateWinner 的函數，用於計算贏家
   const calculateWinner = (squares) => {
     const lines = [
       [0, 1, 2],
@@ -35,30 +45,31 @@ function Board({ xIsNext, squares, onPlay }) {
     return null;
   };
 
+  // 計算贏家
   const winner = calculateWinner(squares);
+  // 設置狀態訊息
   let status = winner
     ? `贏家: ${winner}`
     : `下一個玩家: ${xIsNext ? "X" : "O"}`;
-  // let status;
-  // if (winner) {
-  //   status = "贏家 " + winner;
-  // } else {
-  //   status = "下一個玩家: " + (xIsNext ? "X" : "O");
-  // }
 
+  // 返回 JSX，這部分代碼將會渲染到網頁上
   return (
     <>
+      {/* 顯示狀態訊息 */}
       <div className="status">{status}</div>
+      {/* 渲染第一行的三個 Square 元件 */}
       <div>
         <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
         <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
         <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
       </div>
+      {/* 渲染第二行的三個 Square 元件 */}
       <div>
         <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
         <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
         <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
       </div>
+      {/* 渲染第三行的三個 Square 元件 */}
       <div>
         <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
         <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
@@ -67,4 +78,6 @@ function Board({ xIsNext, squares, onPlay }) {
     </>
   );
 }
+
+// 將 Board 函數元件作為預設導出
 export default Board;
