70961ebe21240313cb1e7be20cf66c29cd7e8266
Lin, Chuan-Jun <144259037+LinChuan-Jun@users.noreply.github.com>
2024-06-19 02:17:46 +0800

    0

--- src/components/MyRGBPanel.js
+++ src/components/MyRGBPanel.js
@@ -1,51 +1,64 @@
+// 從 React 套件中引入 React 和 useState 函數
 import React from "react";
 import { useState } from "react";
+// 引入自定義的 MySlider 元件
 import MySlider from "./MySlider";
 
+// 定義一個名為 MyRGBPanel 的函數元件
 function MyRGBPanel() {
+    // 使用 useState 函數來創建 r, g, b 三個狀態變量，初始值都為 128
     const [r, setR] = useState(128);
     const [g, setG] = useState(128);
     const [b, setB] = useState(128);
 
+    // 定義三個函數分別用來更新 r, g, b 的值
     const updateR = (v) => setR(v);
     const updateG = (v) => setG(v);
     const updateB = (v) => setB(v);
 
+    // 定義一個用於設置 RGB 面板樣式的對象
     const rgbCss = {
-        border: "1px solid blue",
-        borderRadius: "20px",
-        width: "300px",
-        display: "flex",
-        flexDirection: "column",
-        padding: "20px",
-        justifyContent: "center",
-        alignItems: "center",
-        margin: "auto",
-      };
-  return (
-    <>
-      <div
-        style={rgbCss}
-      >
-        <h2 style={{color: "red"}}>
-          目前色彩
-        </h2>
-        <div
-          style={{
-            width: "250px",
-            height: "100px",
-            backgroundColor: `rgb(${r}, ${g}, ${b})`,
-          }}
-        />
-        <span>R: </span>
-        <MySlider onChange={updateR} />
-        <span>G: </span>
-        <MySlider onChange={updateG} />
-        <span>B: </span>
-        <MySlider onChange={updateB} />
-      </div>
-    </>
-  );
+        border: "1px solid blue",  // 設置邊框顏色為藍色
+        borderRadius: "20px",      // 設置邊框圓角
+        width: "300px",            // 設置寬度
+        display: "flex",           // 設置為 flex 布局
+        flexDirection: "column",   // 設置 flex 布局的方向為列方向
+        padding: "20px",           // 設置內邊距
+        justifyContent: "center",  // 設置主軸對齊方式為居中
+        alignItems: "center",      // 設置交叉軸對齊方式為居中
+        margin: "auto",            // 設置自動外邊距，將元素置於容器中央
+    };
+
+    // 返回 JSX，這部分代碼將會渲染到網頁上
+    return (
+        <>
+            {/* 渲染一個 div 元素，並應用 rgbCss 樣式 */}
+            <div style={rgbCss}>
+                {/* 標題元素，顯示 "目前色彩"，字體顏色設置為紅色 */}
+                <h2 style={{color: "red"}}>
+                    目前色彩
+                </h2>
+                {/* 顯示一個 div，用於展示當前的 RGB 顏色 */}
+                <div
+                    style={{
+                        width: "250px",                    // 設置寬度
+                        height: "100px",                   // 設置高度
+                        backgroundColor: `rgb(${r}, ${g}, ${b})`, // 設置背景顏色為當前 RGB 顏色
+                    }}
+                />
+                {/* 顯示 "R: " 並渲染一個 MySlider 元件，當滑動條改變時調用 updateR 函數 */}
+                <span>R: </span>
+                <MySlider onChange={updateR} />
+                {/* 顯示 "G: " 並渲染一個 MySlider 元件，當滑動條改變時調用 updateG 函數 */}
+                <span>G: </span>
+                <MySlider onChange={updateG} />
+                {/* 顯示 "B: " 並渲染一個 MySlider 元件，當滑動條改變時調用 updateB 函數 */}
+                <span>B: </span>
+                <MySlider onChange={updateB} />
+            </div>
+        </>
+    );
 }
 
+// 將 MyRGBPanel 函數元件作為預設導出
 export default MyRGBPanel;
--- src/components/MySlider.js
+++ src/components/MySlider.js
@@ -1,15 +1,31 @@
+// 從 React 套件中引入 useState 函數
 import { useState } from "react";
 
+// 定義一個名為 MySlider 的函數元件，接收一個名為 onChange 的屬性
 function MySlider({onChange}) {
+  // 使用 useState 函數來創建一個名為 value 的狀態變量，初始值為 128
+  // 並且 setValue 是用來更新 value 的函數
   const [value, setValue] = useState(128);
+
+  // 定義一個名為 handleChange 的函數，用於處理 input 元素的 onChange 事件
   const handleChange = (e) => {
+    // 更新 value 的值為當前 input 的值
     setValue(e.target.value);
+
+    // 如果傳入了 onChange 函數，則調用 onChange 並傳遞當前 input 的值
     if (onChange){
       onChange(e.target.value);
     }
   };
+
+  // 返回 JSX，這部分代碼將會渲染到網頁上
   return (
     <div>
+      {/* 
+        定義一個 range 類型的 input 元素，類似於滑動條
+        設定最小值為 0，最大值為 255，寬度為 200
+        值為狀態變量 value，當值改變時會調用 handleChange 函數
+      */}
       <input
         type="range"
         width = "200"
@@ -18,9 +34,11 @@ function MySlider({onChange}) {
         value={value}
         onChange={handleChange}
       />
+      {/* 顯示當前滑動條的值 */}
       <span>{value}</span>
     </div>
   );
 }
 
+// 將 MySlider 函數元件作為預設導出
 export default MySlider;
